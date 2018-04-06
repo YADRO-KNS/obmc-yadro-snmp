5cbe5782e2532bfa1013a0f710de0b91acd0158b
Alexander Filippov <a.filippov@yadro.com>
2018-04-06 11:48:14 +0300

    Add fields for versons of BMC and Host firmware
    Signed-off-by: Alexander Filippov <a.filippov@yadro.com>

---
+++ yadro/versions.cpp
@@ -0,0 +1,27 @@
+/**
+ * @brief Versions buffers implementation
+ *
+ * This file is part of phosphor-snmp project.
+ *
+ * Copyright (c) 2018 YADRO
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author: Alexander Filippov <a.filippov@yadro.com>
+ */
+
+#include "versions.hpp"
+
+char versionBMC[VERSION_MAX_LEN + 1] = "";
+char versionHFW[VERSION_MAX_LEN + 1] = "";
+
