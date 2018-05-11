268ec7368df85c83f69df6bc68a9abf57bcd0c0c
Alexander Filippov <a.filippov@yadro.com>
2018-05-11 18:00:50 +0300

    subagent: remove previous agent implementation
    Signed-off-by: Alexander Filippov <a.filippov@yadro.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,31 +17,7 @@ case $1 in
         done
         exit 0
         ;;
-
-    build)
-        cd ${PREFIX}
-        autoreconf --install && ./configure --enable-tracing && make -C yadro
-        ;;
-
-    run)
-        if [ ! -e "${TARGET}" ]; then
-            echo "Run \"$0 build\" before this action"
-            exit 1
-        fi
-
-        if [ ${EUID} != 0 ]; then
-            echo "This script required root privilegies"
-            sudo $0 $@
-            exit 0
-        fi
-
-        snmpd -f -Le -C -Ddlmod,yadro \
-            --rocommunity=public --dlmod="yadro ${TARGET}" \
-            --trapcommunity=public --trap2sink=localhost
-        ;;
-
-    *)
-        echo "Usage: $0 <build | run | clean>"
-        exit 1
-        ;;
 esac
+
+autoreconf --install ${PREFIX}
+echo 'Run: "./configure ${CONFIGURE_FLAGS} && make"'
--- tests/tracing.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-
-#define TRACE(fmt, ...) printf(fmt, ##__VA_ARGS__)
-#define TRACE_ERROR(fmt, ...) fprintf(stderr, "Error: " fmt, ##__VA_ARGS__)
