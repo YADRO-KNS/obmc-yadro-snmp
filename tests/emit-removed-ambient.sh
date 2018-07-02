2c335ec4eb16b44b39d21bfce9a51208916b714c
Alexander Filippov <a.filippov@yadro.com>
2018-07-02 15:17:26 +0300

    Fix unhandled sdbusplus exceptions
    `sdbusplus` throwed exceptions since commit
    75a2e25322abee603dc51032c734a27bfd4df90d
    
    Signed-off-by: Alexander Filippov <a.filippov@yadro.com>

--- tests/emit-removed-ambient.sh
+++ tests/emit-removed-ambient.sh
@@ -3,10 +3,10 @@
 gdbus emit --system --object-path '/xyz/openbmc_project/sensors'            \
            --signal org.freedesktop.DBus.ObjectManager.InterfacesRemoved    \
            'objectpath "/xyz/openbmc_project/sensors/temperature/ambient"'  \
-           "{                                                               \
+           "[                                                               \
            'org.freedesktop.DBus.Peer',                                     \
            'org.freedesktop.DBus.Introspectable',                           \
            'org.freedesktop.DBus.Properties',                               \
            'xyz.openbmc_project.Sensor.Value'                               \
-           }"
+           ]"
 
