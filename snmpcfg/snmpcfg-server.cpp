7d4c25446a388d8f26ebf927ca7c125f31d4f876
Alexander Filippov <a.filippov@yadro.com>
2022-03-02 16:37:33 +0300

    snmpcfg: Rework snmp-config-manager
    This commit replaces obsolete implementation of SNMP Configuration
    manager.
    
    Now it provides the interface to change SNMPv1/SNMPv2 community name.
    It will be used for manage community name via RedFish.
    
    Signed-off-by: Alexander Filippov <a.filippov@yadro.com>

--- snmpcfg/snmpcfg-server.cpp
+++ snmpcfg/snmpcfg-server.cpp
@@ -26,19 +26,54 @@
 
 #include <sdbusplus/server.hpp>
 #include <xyz/openbmc_project/SNMPCfg/server.hpp>
+#include <phosphor-logging/log.hpp>
+#include <phosphor-logging/elog.hpp>
+#include <phosphor-logging/elog-errors.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
 
+using namespace phosphor::logging;
 using SNMPCfg_inherit = sdbusplus::server::object_t<
     sdbusplus::xyz::openbmc_project::server::SNMPCfg>;
 
-struct Configurator : SNMPCfg_inherit
+using InternalFailure =
+    sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
+using InvalidArgument =
+    sdbusplus::xyz::openbmc_project::Common::Error::InvalidArgument;
+using Argument = xyz::openbmc_project::Common::InvalidArgument;
+
+static constexpr auto snmpdConf = "/etc/snmp/snmpd.conf";
+static constexpr auto whitespace = " \t";
+
+/**
+ * @brief Check if the string contains the token at the position or after
+ *        several spaces.
+ *
+ * @param[in] str   - string to be checked
+ * @param[in] token - expected token
+ * @param[in/out] pos - start position for checking, will keep the last
+ *                      checked position.
+ *
+ * @return - true if token found.
+ */
+static bool getToken(const std::string& str, const std::string& token,
+                     size_t& pos)
 {
-    const std::string snmpd_conf_path = "/etc/snmp/snmpd.conf";
-    const std::string snmpd_orig_path = "/run/initramfs/ro/etc/snmp/snmpd.conf";
+    pos = str.find_first_not_of(whitespace, pos);
+    if (pos != std::string::npos && 0 == str.compare(pos, token.size(), token))
+    {
+        auto endPos = str.find_first_of(whitespace, pos);
+        if (endPos == std::string::npos || pos + token.size() == endPos)
+        {
+            pos = endPos;
+            return true;
+        }
+    }
+    return false;
+}
 
-    const std::string systemd_dst = "org.freedesktop.systemd1";
-    const std::string systemd_path = "/org/freedesktop/systemd1";
-    const std::string systemd_iface = "org.freedesktop.systemd1.Manager";
-
+class Configurator : public SNMPCfg_inherit
+{
+  public:
     /**
      * @brief Configurator object constructor
      *
@@ -46,95 +81,189 @@ struct Configurator : SNMPCfg_inherit
      * @param path - DBus object path
      */
     Configurator(sdbusplus::bus::bus& bus, const char* path) :
-        SNMPCfg_inherit(bus, path)
+        SNMPCfg_inherit(bus, path), bus(bus)
     {
+        readConfig();
     }
 
-    /**
-     * @brief Get actual version of snmpd.conf
-     *
-     * @return snmpd.conf body
-     */
-    virtual std::string getConfig(void) override
+    std::string community(std::string value) override
     {
-        std::ifstream t(snmpd_conf_path);
-        std::string s;
+        static constexpr auto communityMaxLen = 256;
+        static constexpr auto communityMinLen = 1;
 
-        t.seekg(0, std::ios::end);
-        s.reserve(t.tellg());
-        t.seekg(0, std::ios::beg);
-
-        s.assign((std::istreambuf_iterator<char>(t)),
-                 std::istreambuf_iterator<char>());
-
-        return s;
-    }
-
-    /**
-     * @brief Set new version of snmpd.conf
-     *
-     * @param conf - body of snmpd.conf
-     */
-    virtual void setConfig(std::string conf) override
-    {
-        if (dbus_call("StopUnit"))
+        auto communityLen = value.length();
+        if (communityLen < communityMinLen || communityLen > communityMaxLen)
         {
-            std::ofstream t(snmpd_conf_path);
-            t << conf;
-            t.close();
-
-            dbus_call("StartUnit");
+            log<level::ERR>("Invalid community name length");
+            elog<InvalidArgument>(Argument::ARGUMENT_NAME("Community"),
+                                  Argument::ARGUMENT_VALUE(value.c_str()));
         }
-    }
 
-    /**
-     * @brief Rollback snmpd.conf to default version
-     */
-    virtual void resetConfig(void) override
-    {
-        if (dbus_call("StopUnit"))
-        {
-            std::ifstream src(snmpd_orig_path);
-            std::ofstream dst(snmpd_conf_path);
-
-            dst << src.rdbuf();
-
-            dst.close();
-            src.close();
-
-            dbus_call("StartUnit");
-        }
+        SNMPCfg_inherit::community(value);
+        writeConfig();
+        return value;
     }
 
   private:
     /**
-     * @brief Send systemd command over DBus
+     * @brief Parse line and get community name
      *
-     * @param method - Method name (StartUnit | StopUnit)
+     * @param line - the line from configuration file
      *
-     * @return true if success
+     * @return community name if corresponding statement found
+     *         and nullopt otherwise
      */
-    bool dbus_call(const char* method)
+    std::optional<std::string> getCommunityName(std::string line) const
     {
-        auto b = sdbusplus::bus::new_system();
-        auto m = b.new_method_call("org.freedesktop.systemd1",
-                                   "/org/freedesktop/systemd1",
-                                   "org.freedesktop.systemd1.Manager", method);
-        m.append("snmpd.service", "replace");
-        auto r = b.call(m);
-        if (r.is_method_error())
+        // Required line should be in format:
+        // 'com2sec readonly  default     <communityName>'
+        size_t pos = 0;
+
+        for (const auto& token : {"com2sec", "readonly", "default"})
         {
-            fprintf(stderr, "Error: %s() failed!\n", method);
-            return false;
+            if (!getToken(line, token, pos) || pos == std::string::npos)
+            {
+                return std::nullopt;
+            }
         }
 
-#ifdef _DEBUG
-        sdbusplus::message::object_path o;
-        r.read(o);
-        printf("  %s() return '%s'\n", method, o.str.c_str());
-#endif
+        pos = line.find_first_not_of(whitespace, pos);
+        if (pos == std::string::npos)
+        {
+            return std::nullopt;
+        }
 
-        return true;
+        auto endPos = line.find_last_not_of(whitespace);
+        if (pos < endPos && line[pos] == '"' && line[endPos] == '"')
+        {
+            pos++;
+            endPos--;
+        }
+
+        return (pos <= endPos ? line.substr(pos, endPos - pos + 1)
+                              : std::string{});
+    }
+
+    /**
+     * @brief Read actual settings from configuration file
+     */
+    void readConfig()
+    {
+        std::ifstream fileToRead(snmpdConf, std::ios::in);
+        if (!fileToRead.is_open())
+        {
+            log<level::ERR>("Failed to open SNMP daemon configuration file",
+                            entry("FILE_NAME=%s", snmpdConf));
+            return;
+        }
+
+        std::string line;
+        while (std::getline(fileToRead, line))
+        {
+            auto communityName = getCommunityName(line);
+            if (communityName)
+            {
+                SNMPCfg_inherit::community(*communityName);
+                return;
+            }
+        }
+        log<level::ERR>("Community not found",
+                        entry("FILE_NAME=%s", snmpdConf));
+    }
+
+    /**
+     * @brief Write actual settings to the configuration file.
+     */
+    void writeConfig()
+    {
+        std::string tmpFileName{snmpdConf};
+        tmpFileName += ".tmp";
+
+        std::ifstream fileToRead(snmpdConf, std::ios::in);
+        std::ofstream fileToWrite(tmpFileName, std::ios::out);
+        if (!fileToRead.is_open())
+        {
+            log<level::ERR>("Failed to open SNMP daemon configuration file",
+                            entry("FILE_NAME=%s", snmpdConf));
+            return;
+        }
+
+        if (!fileToWrite.is_open())
+        {
+            log<level::ERR>("Failed to create new configuration file",
+                            entry("FILE_NAME=%s", tmpFileName.c_str()));
+            return;
+        }
+
+        auto value = SNMPCfg_inherit::community();
+        bool isQuoteRequired =
+            (value.find_first_of(whitespace) != std::string::npos);
+        bool updated = false;
+        std::string line;
+        while (std::getline(fileToRead, line))
+        {
+            auto communityName = getCommunityName(line);
+            if (communityName && *communityName != value)
+            {
+                fileToWrite << "com2sec readonly  default\t  "
+                            << (isQuoteRequired ? "\"" : "") << value
+                            << (isQuoteRequired ? "\"" : "") << std::endl;
+                updated = true;
+            }
+            else
+            {
+                fileToWrite << line << std::endl;
+            }
+        }
+        fileToWrite.close();
+        fileToRead.close();
+
+        if (updated)
+        {
+            if (0 != std::rename(tmpFileName.c_str(), snmpdConf))
+            {
+                int error = errno;
+                log<level::ERR>("Failed to update SNMP daemon configuarion",
+                                entry("WHAT=%s", strerror(error)),
+                                entry("FILE_NAME=%s", snmpdConf));
+                elog<InternalFailure>();
+            }
+
+            reloadSnmpDaemon();
+        }
+        else
+        {
+            if (0 != std::remove(tmpFileName.c_str()))
+            {
+                int error = errno;
+                log<level::ERR>("Failed to remove temporary file",
+                                entry("WHAT=%s", strerror(error)),
+                                entry("FILE_NAME=%s", tmpFileName.c_str()));
+            }
+        }
+    }
+
+    sdbusplus::bus::bus& bus;
+
+    /**
+     * @brief Ask SNMP daemon to reread configuration.
+     */
+    void reloadSnmpDaemon()
+    {
+        auto m = bus.new_method_call(
+            "org.freedesktop.systemd1", "/org/freedesktop/systemd1",
+            "org.freedesktop.systemd1.Manager", "ReloadUnit");
+        m.append("snmpd.service", "replace");
+        try
+        {
+            bus.call_noreply(m);
+        }
+        catch (const sdbusplus::exception::SdBusError& e)
+        {
+            log<level::ERR>("Failed to reload SNMP daemon",
+                            entry("WHATE=%s", e.what()));
+            elog<InternalFailure>();
+        }
     }
 };
 
